619101059ed3e129f55b6af7ece215c3d742d781
Nordor1 <feliped3carvalho@gmail.com>
2019-01-29 23:54:29 -0200

    included components

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component, Fragment } from "react";
+import ReactDOM from "react-dom";
 
-class App extends React.Component {
+import Header from "./layouts/Header";
+import Dashboard from "./leads/Dashboard";
+
+class App extends Component {
   render() {
-    return <h1>React App</h1>
+    return (
+      <Fragment>
+        <Header />
+        <div className="container">
+          <Dashboard />
+        </div>
+      </Fragment>
+    );
   }
-};
+}
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ frontend/src/components/leads/Dashboard.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from "react";
+import Form from "./Form";
+import Leads from "./Leads";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Form />
+        <Leads />
+      </Fragment>
+    );
+  }
+}
+export default Dashboard;
